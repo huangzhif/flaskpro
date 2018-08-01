1e4c7929c0149593b916f44a726629765c446d0d
huangzhif <394320736@qq.com>
2018-08-01 09:59:19 +0800

    用户信息页和头像

--- app/models.py
+++ app/models.py
@@ -1,10 +1,14 @@
+#encoding:utf-8
 from app import db
+from hashlib import md5
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     @property
     def is_authenticated(self):
@@ -23,6 +27,10 @@ class User(db.Model):
             return unicode(self.id)  # python 2
         except NameError:
             return str(self.id)  # python 3
+
+    #显示头像来源avatar网站注册信息
+    def avatar(self,size):
+        return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
